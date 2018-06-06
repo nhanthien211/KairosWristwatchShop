6a7889582c2747046d702989cd6a44b28ca34822
vyvtt <39051124+vyvtt@users.noreply.github.com>
2018-06-06 23:05:20 +0700

    login v2 not done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,26 +4,62 @@ jQuery(document).ready(function() {
     /*
         Fullscreen background
     */
-    $.backstretch("assets/img/backgrounds/1.jpg");
+    // $.backstretch("images/casio-banner-4.jpg");
     
     /*
-	    Modals
-	*/
-	$('.launch-modal').on('click', function(e){
-		e.preventDefault();
-		$( '#' + $(this).data('modal-id') ).modal();
-	});
-    
-    /*
-        Form validation
+        Forms show / hide
     */
-	$('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+    $('.show-register-form').on('click', function(){
+    	if( ! $(this).hasClass('active') ) {
+    		$('.show-login-form').removeClass('active');
+    		$(this).addClass('active');
+    		$('.login-form').fadeOut('fast', function(){
+    			$('.register-form').fadeIn('fast');
+    		});
+    	}
+    });
+    // ---
+    $('.show-login-form').on('click', function(){
+    	if( ! $(this).hasClass('active') ) {
+    		$('.show-register-form').removeClass('active');
+    		$(this).addClass('active');
+    		$('.register-form').fadeOut('fast', function(){
+    			$('.login-form').fadeIn('fast');
+    		});
+    	}
+    });
+    
+    /*
+        Login form validation
+    */
+    $('.l-form input[type="text"], .l-form input[type="password"], .l-form textarea').on('focus', function() {
     	$(this).removeClass('input-error');
     });
     
-    $('.login-form').on('submit', function(e) {
+    $('.l-form').on('submit', function(e) {
     	
     	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    /*
+        Registration form validation
+    */
+    $('.r-form input[type="text"], .r-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.r-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], textarea').each(function(){
     		if( $(this).val() == "" ) {
     			e.preventDefault();
     			$(this).addClass('input-error');
