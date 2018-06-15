c7cc10c7de50b1b06add5fb54c1090b25c0c70fa
vyvtt <39051124+vyvtt@users.noreply.github.com>
2018-06-15 18:51:26 +0700

    validate admin-info page

--- Scripts/sb-admin.js
+++ Scripts/sb-admin.js
@@ -43,3 +43,36 @@
     event.preventDefault();
   });
 })(jQuery); // End of use strict
+
+// Validate input for form -----------------------------------------------------------
+(function () {
+  'use strict';
+  window.addEventListener('load', function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('to-validate');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+          form.addEventListener('submit', function (event) {
+              if (form.checkValidity() === false) {
+                  event.preventDefault();
+                  event.stopPropagation();
+              }
+              form.classList.add('was-validated');
+          }, false);
+      });
+  }, false);
+})();
+
+// Set max date input is today (DOB)--------------------------------------------------
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0
+var yyyy = today.getFullYear();
+if (dd < 10) {
+  dd = '0' + dd
+}
+if (mm < 10) {
+  mm = '0' + mm
+}
+today = yyyy + '-' + mm + '-' + dd;
+document.getElementById("validationCustom08").setAttribute("max", today);
