a0624f7b6d852dd0521f4c24caca393020346f8d
vyvtt <39051124+vyvtt@users.noreply.github.com>
2018-07-09 13:20:45 +0700

    Add 3 product image

---
+++ ProjectKairos/Utilities/MyCustomUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectKairos.Utilities
+{
+    public class MyCustomUtility
+    {
+        public static string RelativeFromAbsolutePath(string path)
+        {
+            if (HttpContext.Current != null)
+            {
+                var request = HttpContext.Current.Request;
+                var applicationPath = request.PhysicalApplicationPath;
+                var virtualDir = request.ApplicationPath;
+                virtualDir = virtualDir == "/" ? virtualDir : (virtualDir + "/");
+                return path.Replace(applicationPath, virtualDir).Replace(@"\", "/");
+            }
+            return "";
+        }
+    }
+}
